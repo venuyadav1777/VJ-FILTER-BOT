d409ade94a35d23329f9b808c2d638320db2b7b9
venuyadav1777 <youngerbro313@gmail.com>
2024-12-16 16:13:41 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,7 +17,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 
 # This Pictures Is For Start Message Picture, You Can Add Multiple By Giving One Space Between Each.
-PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split()
+PICS = (environ.get('PICS', 'https://graph.org/file/78d01972a9701b51a14b3-9fcef70048b90b24ce.jpg')).split()
 
 
 # Admins & Users
